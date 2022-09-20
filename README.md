b9df77bcc1470df26469c74808e94d998a9f7594
BlackRaser <blackraser17@yandex.ru>
2022-09-21 02:46:14 +0500

    Обновление до 1.1a. Подробности в change.log

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 # MyTanks-Bot
 
-Бот для сервера Discord MyTanks
+Бот для сервера Discord MyTanks. На данный момент доступно несколько простейших комманд
+`ping` - возвращает текущее ожидание сервера на запрос
+`help` - краткое описание бота
+`hello` - команда для того, чтобы поздороваться с ботом
 
 # Идеи
 
 * Краткая Wiki прямо в Discord
 * Создание розыгрышей промокодов
+
+# Используемые материалы
+ https://discord.com/api/oauth2/authorize?client_id=1021841663552913511&permissions=139586956352&scope=bot
+ https://guide.pycord.dev/introduction/
--- main.py
+++ main.py
@@ -1,4 +1,37 @@
-import discord # Импорт библиотек
+#Библиоеки
+from ensurepip import version
+import discord 
 import os
+from colorama import Fore, Back, Style
 from dotenv import load_dotenv
 
+load_dotenv()
+bot = discord.Bot()
+
+@bot.event
+async def on_ready ():
+    print("\n\n################################\n\n#  MyTanks Discord Bot ONLINE  #\n\n################################\n\n")
+
+help = ("Это пока тестовая версия бота. Команды:\n" # Информация о боте
+"`/ping` - Текущая задержка бота\n"
+"`/help` - Команда, которую вы использовали для получения информации\n"
+"`/hello` - Поздороваться с ботом\n\n"
+"Версия: "  + os.getenv('version'))
+
+class comands: # Команды
+    @bot.slash_command (name = "hello", description = "Скажи привет боту!")
+    async def hello(ctx):
+        await ctx.respond(f"Привет, {ctx.author}!")
+        print(f" * hello использовал пользователь {ctx.author}")
+
+    @bot.slash_command (name = 'help', descripton = 'Информация о боте')
+    async def help(ctx):
+        await ctx.respond(help)
+        print(f" * version использовал пользователь {ctx.author}")
+
+    @bot.command(name = 'ping', description="Возвращает задержку бота") # this decorator makes a slash command
+    async def ping(ctx): # a slash command will be created with the name "ping"
+        await ctx.respond(f"Ping!\nLatency is **{round(bot.latency * 1000)} ms**!")
+        print(f" * ping использовал пользователь {ctx.author}")
+
+bot.run(os.getenv('TOKEN'))
