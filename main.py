793fc194a647b04a480672374c9bdc6f4e292406
BlackRaser <blackraser17@yandex.ru>
2022-12-08 02:57:44 +0500

    Revert "1.3 alpha"
    This reverts commit 9f0c483c51ca73d7da804b7ce3f809cc147e2cb4.

--- main.py
+++ main.py
@@ -1,47 +1,37 @@
-#Lib
-import datetime
-import logging
-import os
+#Библиоеки
 from ensurepip import version
-
-import discord
-from colorama import Back, Fore, Style
+import discord 
+import os
+from colorama import Fore, Back, Style
 from dotenv import load_dotenv
-
-#Logging
-logging.basicConfig(level=logging.INFO, filename="events.log",filemode="w", format="%(asctime)s %(levelname)s %(message)s")
 
 load_dotenv()
 bot = discord.Bot()
 
 @bot.event
 async def on_ready ():
-    print("\nMyTanks Discord Bot ONLINE\n")
+    print("\n\n################################\n\n#  MyTanks Discord Bot ONLINE  #\n\n################################\n\n")
 
-#Help
-help = (os.getenv('help') + "v.: "  + os.getenv('version') + "." + os.getenv('build'))
+help = ("Это пока тестовая версия бота. Команды:\n" # Информация о боте
+"`/ping` - Текущая задержка бота\n"
+"`/help` - Команда, которую вы использовали для получения информации\n"
+"`/hello` - Поздороваться с ботом\n\n"
+"v.: "  + os.getenv('version') + "." + os.getenv('build'))
 
-#Commands
-class comands:
-    @bot.command (name = "hello", description = "Say hello to the bot!")
+class comands: # Команды
+    @bot.slash_command (name = "hello", description = "Скажи привет боту!")
     async def hello(ctx):
-        author = str(ctx.author.id)
-        await ctx.respond(f"Hi, {ctx.author.mention}")
-        print(f" * hello использовал пользователь " + author)
-        logging.info(f"Command 'hello' using by " + author)
+        await ctx.respond(f"hi, {ctx.author.mention}")
+        print(f" * hello использовал пользователь {ctx.author}")
 
-    @bot.command (name = 'help', descripton = 'Information about bot')
+    @bot.slash_command (name = 'help', descripton = 'Информация о боте')
     async def help(ctx):
-        author = str(ctx.author.id)
         await ctx.respond(help)
-        print(f" * help использовал пользователь " + author)
-        logging.info(f"Command 'help' using by " + author)
+        print(f" * help использовал пользователь {ctx.author}")
 
     @bot.command(name = 'ping', description="Возвращает задержку бота") 
     async def ping(ctx): 
-        author = str(ctx.author.id)
         await ctx.respond(f"Ping!\nLatency is **{round(bot.latency * 1000)} ms**!")
-        print(f" * ping использовал пользователь " + author)
-        logging.info(f"Command 'ping' using by " + author)
+        print(f" * ping использовал пользователь {ctx.author}")
 
 bot.run(os.getenv('TOKEN'))
