7c16426a90ef561eb42b2f2d43c076df49c856f6
BlackRaser <blackraser17@yandex.ru>
2023-01-08 16:08:28 +0500

    1.8.28a
    - В логах теперь отображается ник автора запроса

--- Bot.py
+++ Bot.py
@@ -22,7 +22,7 @@ load_dotenv()
 ##### VAR #####
 intents = discord.Intents.all()
 activity = discord.Game(name=f"/help | mytanks.net")
-bot = discord.Bot(command_prefix="/", activity=activity, status=discord.Status.idle, intents = intents)
+bot = discord.Bot(command_prefix=".", activity=activity, status=discord.Status.idle, intents = intents)
 
 ver = (os.getenv('version') +"."+ os.getenv('build') )
 event = "USED:      Command "
@@ -57,9 +57,7 @@ class embeds:
         url=f"{os.getenv('site')}", 
         icon_url='https://cdn.discordapp.com/attachments/942924556555927612/1052299135669243924/MyTanks.png'
     )
-    help.set_footer(
-        text = f"ver.:{ver}  |  Powered by MyTanks  |  Last update {os.getenv('upd_date')}"
-    ) 
+    help.set_footer(text = f"ver.:{ver}  |  Powered by MyTanks  |  Last update {os.getenv('upd_date')}") 
 
     # FAQ
     faq=discord.Embed(
@@ -71,13 +69,17 @@ class embeds:
         color=0x38f11a
     )
     faq.add_field(
+        name="--------------------------------------------------",
+        value=":flag_ru:  RU"
+    )
+    faq.add_field(
         name="В игре читер, игра сломалась, забыл пароль и т.д.", 
         value="Напиши в канал <#847887130415595561>. Там тебе помогут", 
         inline=False
     )
     faq.add_field(
         name="--------------------------------------------------",
-        value="                         EN"
+        value=":flag_gb:  EN"
     )
     faq.add_field(
         name="The game is a cheater, the game broke, forgot the password, etc.", 
@@ -89,9 +91,7 @@ class embeds:
         url=f"{os.getenv('site')}", 
         icon_url='https://cdn.discordapp.com/attachments/942924556555927612/1052299135669243924/MyTanks.png'
     )
-    faq.set_footer(
-        text = f"ver.:{ver}  |  Powered by MyTanks  |  Last update {os.getenv('upd_date')}"
-    )
+    faq.set_footer(text = f"ver.:{ver}  |  Powered by MyTanks  |  Last update {os.getenv('upd_date')}")
     
     # Stop
     stop=discord.Embed(
@@ -103,9 +103,25 @@ class embeds:
         url=f"{os.getenv('site')}", 
         icon_url='https://cdn.discordapp.com/attachments/942924556555927612/1052299135669243924/MyTanks.png'
     )
-    stop.set_footer(
-        text = f"ver.:{ver}  |  Powered by MyTanks  |  Last update {os.getenv('upd_date')}"
-    ) 
+    stop.set_footer(text = f"ver.:{ver}  |  Powered by MyTanks  |  Last update {os.getenv('upd_date')}") 
+
+
+
+##### Buttons #####
+class button(discord.ui.View): 
+        def __init__(self, *, timeout=1800):
+            super().__init__(timeout=timeout)
+       
+class faq_button(discord.ui.View): 
+        def __init__(self, *, timeout=1800):
+            super().__init__(timeout=timeout)
+        @discord.ui.button(label="FAQ",style=discord.ButtonStyle.red,emoji="❔")
+        async def red_button(self,button:discord.ui.Button,interaction:discord.Interaction,custom_id = "faq"):
+            bfaq=faq_button()
+            bfaq.add_item(discord.ui.Button(label="x32 Client",style=discord.ButtonStyle.link,url="https://cdn.discordapp.com/attachments/847886330301907014/900783835103514654/MyTanks.exe"))
+            await interaction.response.send_message(embed=embeds.faq, view=bfaq)  
+
+
 
 # Start bot
 @bot.event
@@ -115,34 +131,22 @@ async def on_ready ():
 
 
 
-##### Buttons ##### 
-class faq_button(discord.ui.View): 
-        def __init__(self, *, timeout=1800):
-            super().__init__(timeout=timeout)
-        @discord.ui.button(label="FAQ",style=discord.ButtonStyle.red,emoji="❔")
-        async def red_button(self,button:discord.ui.Button,interaction:discord.Interaction,custom_id = "faq"):
-            await interaction.response.send_message(content='/faq')
-
-class button(discord.ui.View): 
-        def __init__(self, *, timeout=1800):
-            super().__init__(timeout=timeout)    
-
-
-
 ##### Commands #####
 class comands:
 
 # Help
-    @bot.command (name = "help", description = "Информация о боте и помощь | Information about bot and help")
+    @bot.command(name = "help", description = "Информация о боте и помощь | Information about bot and help")
     async def help (ctx):
-        author = str(ctx.author.id)
+        author_id = str(ctx.author.id)
+        author_name=str(ctx.author.name)
+        author=str(author_id + " | " + author_name)
 
         # Buttons
-        view=faq_button()
-        view.add_item(discord.ui.Button(label="Oficial Website",style=discord.ButtonStyle.link,url=f"{os.getenv('site')}"))
-        view.add_item(discord.ui.Button(label="VK",style=discord.ButtonStyle.link,url="https://vk.com/mytanksonline_official"))
+        faq=faq_button()
+        faq.add_item(discord.ui.Button(label="Oficial Website",style=discord.ButtonStyle.link,url=f"{os.getenv('site')}"))
+        faq.add_item(discord.ui.Button(label="VK",style=discord.ButtonStyle.link,url="https://vk.com/mytanksonline_official"))
         
-        await ctx.respond(embed=embeds.help,view=view)
+        await ctx.respond(embed=embeds.help,view=faq)
         
         # Logging
         print(Fore.YELLOW, f" * help использовал пользователь {author}", Style.RESET_ALL)
@@ -151,15 +155,23 @@ class comands:
 # FAQ
     @bot.command(name = "faq", description = "Часто задаваемые вопросы | Frequently asked questions")
     async def faq(ctx):
-        author = str(ctx.author.id)
+        author_id = str(ctx.author.id)
+        author_name=str(ctx.author.name)
+        author=str(author_id + " | " + author_name)
         bfaq=button()
         bfaq.add_item(discord.ui.Button(label="x32 Client",style=discord.ButtonStyle.link,url="https://cdn.discordapp.com/attachments/847886330301907014/900783835103514654/MyTanks.exe"))
-        await ctx.respond(embed=embeds.faq,view=bfaq)      
+        await ctx.respond(embed=embeds.faq,view=bfaq)
+        
+        #Logging
+        print(Fore.YELLOW, f" * faq использовал пользователь {author}", Style.RESET_ALL)
+        logging.warning(f"{event} 'faq' by {author}")       
         
 # Hello        
     @bot.command (name = "hello", description = "Скажи привет боту! | Say hello to the bot!")
     async def hello(ctx):
-        author = str(ctx.author.id)
+        author_id = str(ctx.author.id)
+        author_name=str(ctx.author.name)
+        author=str(author_id + " | " + author_name)
         await ctx.respond(f"Hi, {ctx.author.mention}")
         
         # Logging
@@ -169,7 +181,9 @@ class comands:
 # Ping
     @bot.command(name = "ping", description="Возвращает задержку бота | Returns the bot delay") 
     async def ping(ctx): 
-        author = str(ctx.author.id)
+        author_id = str(ctx.author.id)
+        author_name=str(ctx.author.name)
+        author=str(author_id + " | " + author_name)
         latency = round(bot.latency*1000)
         latency=str(latency)
         print(Fore.MAGENTA, f'Current latency {latency}', Style.RESET_ALL)
@@ -182,10 +196,12 @@ class comands:
         logging.warning(f"{event} 'ping' by {author}")
 
 # Stop    
-    @bot.command(name = "stop", description="Останавливает бота | Stops the bot")
+    @bot.command(name = "stop", description="Останавливает бота | Stops the bot", hidden=True)
     async def stop (ctx):
-        author = str(ctx.author.id)
-        if author==os.getenv("admin_id"):
+        author_id = str(ctx.author.id)
+        author_name=str(ctx.author.name)
+        author=str(author_id + " | " + author_name)
+        if author_id==os.getenv("admin_id"):        #check
             # Logging
             print (Fore.RED, "\n====================== MyTanks Discord Bot OFFLINE ======================\n", Style.RESET_ALL)
             logging.warning("\n\n====================== MyTanks Discord Bot OFFLINE ======================\n")
@@ -198,6 +214,9 @@ class comands:
             # Logging
             print(Fore.YELLOW, f" * stop использовал пользователь {author}", Style.RESET_ALL)
             logging.error(f"{event} 'stop' by {author}")
+            
+#Wiki
+    ""
 
 
 bot.run(os.getenv('TOKEN'))
